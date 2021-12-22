6f63868a29f12dbf16d5417272cf961ab1bfa4a3
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-12-22 21:23:14 +0700

    UPDATE 20211222 21:07

--- app/src/main/java/com/ims_hr/latihan6/HasilActivity.java
+++ app/src/main/java/com/ims_hr/latihan6/HasilActivity.java
@@ -2,8 +2,9 @@ package com.ims_hr.latihan6;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DecimalFormat;
 
--- app/src/main/java/com/ims_hr/latihan6/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan6/MainActivity.java
@@ -2,10 +2,11 @@ package com.ims_hr.latihan6;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
